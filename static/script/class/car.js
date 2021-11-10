0188c3fe5177caa249c35a771b6e3eab63dcaaf0
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2021-11-10 14:55:58 -0600

    little changes

--- static/script/class/car.js
+++ static/script/class/car.js
@@ -1,7 +1,8 @@
 var car = /** @class */ (function () {
     function car(ctx, canvas) {
-        this.velocity = 1;
+        this.velocity = 1.5;
         this.x = 0;
+        this.scale = 17;
         this.movement(ctx, canvas);
     }
     car.prototype.movement = function (ctx, canvas) {
@@ -11,7 +12,7 @@ var car = /** @class */ (function () {
         ctx.fillRect(0, 0, canvas.width, canvas.height);
         var image = new Image();
         image.src = "image/car.png";
-        ctx.drawImage(image, this.x, canvas.height - 100, 100, 100);
+        ctx.drawImage(image, this.x, canvas.height - image.height / this.scale, image.width / this.scale, image.height / this.scale);
         setTimeout(function () { requestAnimationFrame(_this.movement.bind(_this, ctx, canvas)); }, 100);
     };
     return car;
--- static/script/class/car.ts
+++ static/script/class/car.ts
@@ -1,16 +1,19 @@
 export class car {
-    velocity: number = 1;
+    public velocity: number = 1.5;
     x: number = 0;
+    scale:number=17
     constructor(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
         this.movement(ctx, canvas)
     }
-    movement(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
+    public movement(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
         this.x += this.velocity;
         ctx.fillStyle = "#000"
         ctx.fillRect(0, 0, canvas.width, canvas.height)
         let image = new Image();
         image.src = "image/car.png"
-        ctx.drawImage(image, this.x, canvas.height - 100, 100, 100)
+        
+        ctx.drawImage(image, this.x, canvas.height-image.height/this.scale, image.width/this.scale, image.height/this.scale)
+        
         setTimeout(() => { requestAnimationFrame(this.movement.bind(this, ctx, canvas)) }, 100)
 
     }
