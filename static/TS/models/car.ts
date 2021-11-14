2ff509050e94149a0b2a3e823afa4009ca2c30e2
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2021-11-13 22:07:55 -0600

    una mejor escalabilidad

--- static/TS/models/car.ts
+++ static/TS/models/car.ts
@@ -1,9 +1,9 @@
 export class car {
-  public velocity: number = 1.5;//meters per second
+  public velocity: number = 40;//meters per second
 
   private x: number = -100;
   private scale: number = 17;
-  distance:number=1000;
+  distance:number=500;
 
   constructor(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement, velocity: number) {
     this.velocity = velocity;
@@ -12,18 +12,23 @@ export class car {
   }
 
   public movement(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement): void {
-    let image = new Image();
-  
-    this.x += this.velocity*(canvas.width/this.distance);// con esto simplemente escalo el tamaño del canvas para que tenga sentido
-  
-    ctx.fillStyle = "#000"
+    
     ctx.fillRect(0, 0, canvas.width, canvas.height)
-
-
+    let image = new Image();
     image.src = "img/car.png"
+    
+    // esto solo es para escalarlo de mejor manera
+
+    this.x += (this.velocity*(canvas.width/this.distance))/100;// la velocidad esta en metros por segundo , en un segundo hay 100 frames asi que lo divido entre 100 ya que un segundo tiene 1000 milisegundos 
     let height= (image.height / (this.scale))/(this.distance/1000)
     let width=(image.width / (this.scale))/(this.distance/1000)
-
+    
+    
+    
+    ctx.fillStyle = "#000"
+    
+    
+    
     ctx.drawImage(
       image, this.x-width, canvas.height -height,width,
       (image.height / (this.scale))/(this.distance/1000)
